27c4b501f46b2258afc7c9422c05df681f0c7daf
Trivium0911 <triv_1993@mail.ru>
2022-04-13 14:43:49 +0300

    added time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 def test_basket_button_present(browser):
     browser.implicitly_wait(10)
     browser.get(link)
+    time.sleep(30)
     btn = browser.find_element(By.CSS_SELECTOR, "#content_inner > article > div.row > div.col-sm-6.product_main > button")
-    time.sleep(10)
     assert btn is not None, "Basket not found."
